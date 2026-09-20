916909a80ea8a607a3d2bc0e3f45965051ce2c9b
newdolph <alexharri@meiliboxi.fi>
2026-09-20 21:18:26 +0300

    Delete 01-developer.ps1

--- sets/01-developer.ps1
+++
@@ -1,3 +0,0 @@
-**01-developer.ps1**
-```powershell
-winget install -e --id Microsoft.VisualStudioCode Git.Git OpenJS.NodeJS.LTS Python.Python.3.12 Docker.DockerDesktop Microsoft.WindowsTerminal Microsoft.PowerToys 7zip.7zip Notepad++.Notepad++ --accept-package-agreements --accept-source-agreements
