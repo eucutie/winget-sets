d0121f6a3cfd595b425ffa54a5e11486e4eaf3de
newdolph <alexharri@meiliboxi.fi>
2026-09-20 21:13:27 +0300

    Create 01-developer.ps1

---
+++ sets/01-developer.ps1
@@ -0,0 +1,3 @@
+**01-developer.ps1**
+```powershell
+winget install -e --id Microsoft.VisualStudioCode Git.Git OpenJS.NodeJS.LTS Python.Python.3.12 Docker.DockerDesktop Microsoft.WindowsTerminal Microsoft.PowerToys 7zip.7zip Notepad++.Notepad++ --accept-package-agreements --accept-source-agreements
